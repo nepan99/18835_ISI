5e77893585052e295ad3d7de8e564353f03d5635
Nepan <50147006+nepan99@users.noreply.github.com>
2020-11-04 19:58:58 +0000

    Projeto_Metereologia

---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ISI_IPMAForm
+{
+    public partial class Form1 : Form
+    {
+        // Variáveis globais
+        static Dictionary<int, string> Locais { get; set; }
+
+        /// <summary>
+        /// Leitura do ficheiro com a informação acerca dos locais: locais.csv
+        /// </summary>
+        /// <param name="ficheiro">caminho ficheiro .csv</param>
+        static Dictionary<int, string> LerLocais(string ficheiro)
+        {
+            Dictionary<int, string> dicLocais = new Dictionary<int, string>();
+
+            // Expressão Regular para instanciar objeto Regex
+            String erString = @"^[0-9]{7},[123],([1-9]?\d,){2}[A-Z]{3},([^,\n]*)$";
+            Regex g = new Regex(erString);
+
+            using (StreamReader r = new StreamReader(ficheiro))
+            {
+                string line;
+
+                while ((line = r.ReadLine()) != null)
+                {
+                    // Tenta correspondência (macthing) da ER com cada linha do ficheiro
+                    Match m = g.Match(line);
+
+                    // Se não corresponder salta para a próxima iteração
+                    if (!m.Success) continue;
+                    
+                    string[] campos = m.Value.Split(',');
+                    int codLocal = int.Parse(campos[0]);
+                    string cidade = campos[5];
+                    dicLocais.Add(codLocal, cidade);
+                }
+            }
+
+            return dicLocais;
+        }
+
+        static PrevisaoIPMA LerFicheiroPrevisao(int globalIdLocal)
+        {
+            String jsonString = null;
+            using (StreamReader reader = new StreamReader(@"data_forecast/"+ globalIdLocal  + ".json"))
+                jsonString = reader.ReadToEnd();
+
+            PrevisaoIPMA obj = JsonConvert.DeserializeObject<PrevisaoIPMA>(jsonString);
+
+            // Guardar local da previsão
+            if (Locais.ContainsKey(obj.globalIdLocal))
+                obj.local = Locais[obj.globalIdLocal];
+            else
+                obj.local = "Não detetado";
+
+            return obj;
+        }
+
+        static void GuardarOutputs(List<PrevisaoIPMA> listaPrevisoes)
+        {
+            string json;
+            foreach (PrevisaoIPMA previsao in listaPrevisoes)
+            {
+                json = JsonConvert.SerializeObject(previsao);
+
+                // Verificar se o diretory output existe, se não cria.
+                if (!Directory.Exists("output/")) Directory.CreateDirectory("output/");
+
+                File.WriteAllText("output/" + previsao.globalIdLocal + ".json", json);
+                
+                XmlDocument xmlNode = JsonConvert.DeserializeXmlNode(json, "root");
+                xmlNode.Save("output/" + previsao.globalIdLocal + ".xml");
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Locais = LerLocais("data_forecast/locais.csv");
+            List<PrevisaoIPMA> listaPrevisoes = new List<PrevisaoIPMA>();
+            int idPrevisao;
+
+            foreach (string previsao in Directory.GetFiles("data_forecast/"))
+            {
+                if (!previsao.EndsWith(".json") || 
+                    !int.TryParse(Path.GetFileNameWithoutExtension(previsao), out idPrevisao)) continue;
+
+                listaPrevisoes.Add(LerFicheiroPrevisao(idPrevisao));
+            }
+
+            // Guardar outputs
+            GuardarOutputs(listaPrevisoes);
+        }
+    }
+}
---
+++ IPMA.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISI_IPMAForm
+{
+    public class PrevisaoDia
+    {
+        public string precipitaProb { get; set; }
+        public string tMin { get; set; }
+        public string tMax { get; set; }
+        public string predWindDir { get; set; }
+        public int idWeatherType { get; set; }
+        public int classWindSpeed { get; set; }
+        public string longitude { get; set; }
+        public string forecastDate { get; set; }
+        public int classPrecInt { get; set; }
+        public string latitude { get; set; }
+    }
+
+    class PrevisaoIPMA
+    {
+        public string owner { get; set; }
+        public string country { get; set; }
+        public PrevisaoDia[] data { get; set; }
+        public int globalIdLocal { get; set; }
+        public DateTime dataUpdate { get; set; }
+
+        // ---- 
+        public string local { get; set; }
+    }
+}
